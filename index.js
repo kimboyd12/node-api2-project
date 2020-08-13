284d3da09428a20b58f303e8783b37c9f0cc34a5
kim-boyd <kimberlyboyd2@gmail.com>
2020-08-13 18:16:05 -0400

    env var for stretch

--- index.js
+++ index.js
@@ -8,6 +8,16 @@ const port = process.env.PORT || 4001
 server.use(express.json())
 server.use(postsRouter)
 
+server.get('/', (req, res) => {
+    try {
+        const messageOfTheDay = process.env.MOTD || 'Hello, World!'
+        res.status(200).json({ motd: messageOfTheDay})
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: "Can't retrieve message of the day!" })
+    }
+})
+
 
 server.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`)
